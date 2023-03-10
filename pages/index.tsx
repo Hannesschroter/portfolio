2a13362aadfc6d87bff854d7edc40481f667a64f
hannesschroter <hannes@schroter.biz>
2023-03-10 17:42:21 +0100

    Bug Fixes Build

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Header from "../components/Header";
 import Hero from '@/components/Hero';
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
-        <title>Hannes' Portfolio</title>
+        <title>Hannes Schroter</title>
       </Head      >
 
       <Header />
@@ -24,7 +22,7 @@ export default function Home() {
       
       {/* About */}
       <section>
-        
+
       </section>
       
       {/* Experiences */}
