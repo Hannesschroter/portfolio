a6c1aedac8c55582146f296027f963cec81310dd
hannesschroter <hannes@schroter.biz>
2023-07-08 15:41:03 +0200

    added contactme

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { AiOutlinePhone, AiOutlineMail } from 'react-icons/ai';
+import { PiMapPinFill } from 'react-icons/pi';
+
+
+type Inputs = {
+    name: string;
+    email: string;
+    subject: string;
+    message: string;
+  };
 
 type Props = {};
 
 function Contact({}: Props) {
+    const {
+        register,
+        handleSubmit,
+      } = useForm<Inputs>()
+    const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+
+
+
   return (
     <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row
     max-w-7xl px-10 justify-evenly mx-auto items-center">
@@ -11,12 +30,47 @@ function Contact({}: Props) {
         </h3>
         <div className="flex flex-col space-y-10">
             <h4 className="text-4xl font-semibold text-center" >
-                I have got just what you need.
-
+                I have got just what you need.{" "}
+                <span className="decoration-[#F7AB0A]/50 underline"> Let's talk</span>
             </h4>
+            <div className="space-y-10">
+                <div className="flex items-center space-x-5 justify-center">
+                    <AiOutlinePhone className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+                    <p className="text-2xl">+49 174 7717828</p>
+                </div>
+
+                <div className="flex items-center space-x-5 justify-center">
+                    <AiOutlineMail className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+                    <p className="text-2xl">hannes@schroter.biz</p>
+                </div>
+
+                <div className="flex items-center space-x-5 justify-center">
+                    <PiMapPinFill className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+                    <p className="text-2xl">Heidelberg</p>
+                </div>
+            </div>
+
+            <form className="flex flex-col space-y-2 w-fit mx-auto">
+                <div className="flex space-x-2">
+                    <input placeholder="Name" className="contactInput" type="text" />
+                    <input placeholder="Email" className="contactInput" type="email" />
+                </div>
+
+                <input placeholder="Subject" className="contactInput" type="text" />
+
+                <textarea placeholder="Message" className="contactInput" />
+                <button 
+                type="submit" 
+                className="bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold
+                text-lg">
+                    Submit
+                </button>
+
+            </form>
+
         </div>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
