c242d3c547c34ceba81441f3721c8a7f1f5961b3
hannesschroter <hannes@schroter.biz>
2023-07-18 15:04:12 +0200

    Moving Circle Feature

--- components/CirclesPulse.tsx
+++ components/CirclesPulse.tsx
@@ -5,7 +5,7 @@ type Props = {}
 function Circles({}: Props) {
   return (
     <div className="relative flex justify-center items-center">
-      <div className="absolute left-1/2 top-2 h-[1512px] w-[1512px] -translate-x-1/3 stroke-gray-300/70 [mask-image:linear-gradient(to_bottom, red_80%,transparent_10%)] sm:top-16 sm:-translate-x-1/2 lg:-top-16 lg:ml-12 xl:-top-14 xl:ml-0">
+      <div className="absolute left-1/2 top-4 md:h-[1512px] md:h-[1512px] xl:w-[1512px] sm:h-[1100px] sm:w-[1100px] -translate-x-1/3 stroke-gray-300/70 mask-image:linear-gradient(to_bottom, red,transparent_10%) sm:-top-64 sm:-translate-x-1/2 lg:-top-24 lg:ml-12 xl:-top-96 xl:ml-0">
 
         <div className="absolute inset-0 h-full w-full circle_large z-0"></div>
         <div className="absolute inset-0 h-4/6 w-4/6 circle_small z-10 self-center  m-auto"></div>
