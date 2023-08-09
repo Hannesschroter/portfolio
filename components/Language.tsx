bf5c91ee5fb7973554b3aacc606fb7518575316e
hannesschroter <hannes@schroter.biz>
2023-08-09 13:51:16 +0200

    added languages

---
+++ components/Language.tsx
@@ -0,0 +1,41 @@
+import { motion } from 'framer-motion';
+import React from 'react';
+import { urlFor } from '@/sanity';
+
+
+type Props = {
+    directionLeft?: boolean;
+    language: string;
+    progress: number;
+    url: string;
+}
+
+function Language({language, progress, url, directionLeft}: Props) {
+  return (
+    <div className="group relative flex cursor-pointer">
+        <motion.img 
+        initial={{
+          x: directionLeft ? -200 : 200,
+          opacity: 0,
+        }}
+        transition={{ duration: 1 }}
+        whileInView={{ opacity: 1, x: 0}}
+        src={url}
+        alt=""
+        className="rounded-full shadow-gray-900 shadow-sm object-cover h-[75px] w-[75px] md:h-28 md:w-28 xl:h-32 xl:w-32 rounded-full
+        filter transition duration-300 ease-in-out" />
+        
+        <div className="absolute hover:backdrop-filter hover:backdrop-blur-sm hover:bg-opacity-30 opacity-0 hover:opacity-100 transition duration ease-in-out
+        hover:bg-[#0e0e0d] h-[75px] w-[75px] md:h-28 md:w-28 xl:h-32 xl:w-32 rounded-full z-1">
+            <div className="flex flex-col items-center text-center justify-center h-full z-0">
+                <p className="text-sm text-slate-100 opacity-100"> { language }  </p>
+                <p className="text-base tracking-wide text-slate-100 opacity-100">{progress}%</p>                
+            </div>
+        </div> 
+
+    </div>
+    
+  );
+}
+
+export default Language;
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,6 +1,7 @@
 import { motion } from 'framer-motion';
 import React from 'react';
 import Skill from './Skill';
+import Language from './Language';
 import { Skill as SkillType } from '@/typings';
 
 type Props = {
@@ -11,19 +12,32 @@ function Skills({skills}: Props) {
   return (
     <motion.div className="flex relative overflow-hidden flex-col text-lft
     pt-10 px-1 mx-auto md:text-left max-w-[2000px]
-    xl:px-10 min-h-screen xl:space-y-0 mx-auto items-center">
-    <h3 className="mt-10 uppercase tracking-[20px] text-gray-500 text-2xl">Skills</h3>
+    min-h-screen mx-auto items-center">
+    <h3 className="mt-10 md:mt-10 uppercase tracking-[20px] text-gray-500 text-2xl">Skills</h3>
     <h3 className="mt-10 uppercase tracking-[3px]">Click on a skill for current proficiency</h3>
-    <h4 className="mt-10 uppercase tracking-[3px]"> Languages </h4>
-    <div className="mx-2 mt-10 grid grid-cols-4 gap-4 justify-evenly">
+    <h4 className="mt-10 md:mt-10 uppercase tracking-[3px]"> Languages </h4>
+    <div className="mx-2 mt-10 grid grid-cols-3 gap-4 md:gap-6 md:mt-10 justify-evenly">
+      <Language language="German" progress={100} url="/germany.jpg" />
+      <Language language="English" progress={80} url="/English.jpg"/>
+      <Language language="Spanish" progress={85} url="/Spanien.jpg"/>
+    </div>
+
+    <h4 className="mt-10 md:mt-10 uppercase tracking-[3px]"> Programming Languages </h4>
+    <div className="mx-2 mt-10 grid grid-cols-4 gap-4 md:gap-6 md:mt-10 justify-evenly">
+      <Language language="Python" progress={80} url="/python.png" />
+      <Language language="Java" progress={50} url="/Java.jpg"/>
+      <Language language="JavaScript" progress={30} url="/javascript.png"/>
+      <Language language="SQL" progress={30} url="/sql.jpg"/>
+    </div>
+
+    <h4 className="mt-10 md:mt-10 uppercase tracking-[3px]"> Technology </h4> 
+    <div className="mx-2 mt-10 grid grid-cols-4 gap-4 md:gap-6 justify-evenly">
       {skills?.map(skill => (
         <Skill key={skill._id} skill={skill}/>
-
-
       )   
       )}
-
     </div>
+
     
     
     </motion.div>
